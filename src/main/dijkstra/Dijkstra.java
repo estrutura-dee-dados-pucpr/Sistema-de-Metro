e84af2bd74d7072026e0d511e51cf8aa0d711ac2
Bruno Pastre <pastre68@gmail.com>
2017-11-12 03:25:41 -0200

    Update Dijkstra.java

--- src/main/dijkstra/Dijkstra.java
+++ src/main/dijkstra/Dijkstra.java
@@ -7,7 +7,7 @@ public class Dijkstra {
 	private Rota[][] rotas;
 	private int inicio, fim;
 
-	public Dijkstra(Rota [] rota, int inicio, int fim) {
+	public Dijkstra(Rota[] rota, int inicio, int fim) {
 		this.rotas = initMatriz(rota);
 		this.inicio = inicio;
 		this.fim = fim;
@@ -15,7 +15,7 @@ public class Dijkstra {
 
 	public static int pesoDaRota(ArrayList<Rota> rota) {
 		int res = 0;
-		if(rota.isEmpty())
+		if (rota.isEmpty())
 			return 0;
 		for (Rota r : rota) {
 			res += r.getPeso();
@@ -29,7 +29,7 @@ public class Dijkstra {
 		StringBuilder builder = new StringBuilder();
 		int tempoRes;
 		tempoRes = Dijkstra.pesoDaRota(rotaMaisCurta());
-		if(this.rotaMaisCurta().isEmpty())
+		if (this.rotaMaisCurta().isEmpty())
 			return "f";
 		for (Rota r : this.rotaMaisCurta()) {
 			builder.append(r + "\n");
@@ -39,32 +39,37 @@ public class Dijkstra {
 		return builder.toString();
 	}
 
-	public ArrayList<Rota> rotaMaisCurta(){
+	public ArrayList<Rota> rotaMaisCurta() {
 		ArrayList<Rota> res = new ArrayList<Rota>();
 		resolve(this.inicio, new Pilha(), res);
 		return res;
 	}
 
+	///TODO: FIX BUG DE 1 A 12
 	private void resolve(int inicio, Pilha tempRes, ArrayList<Rota> currRes) {
 
-		//System.out.println(String.format("Saida de: %d ate %d\n", inicio, fim));
+		// System.out.println(String.format("Saida de: %d ate %d\n", inicio, fim));
 		Rota[] lin = rotas[inicio];
-		if(tempRes== null)
+		if (tempRes == null)
 			tempRes = new Pilha();
 
 		for (int i = 0; i < lin.length; i++) {
-			//if(lin[i] != null)
-			//	System.out.println("FLAG: " + lin[i].getFlag());
-			if(lin[i] != null && !lin[i].getFlag()) {
+
+			if (lin[i] != null && !lin[i].getFlag()) {
+				// if(lin[i].getSaida() != inicio)
+				// break;
+
+
 				tempRes.push(lin[i]);
 				lin[i].setFlag(true);
-				System.out.println(String.format("Estou em linha %d coluna %d com peso %d\n", inicio, i, lin[i].getPeso()));
-				//System.out.println("Pushed! TEMPRES: " + tempRes);
-				if(tempRes.getLast().getSaida() - 1 == fim) {
-					System.out.println("Achei rota: " + tempRes + "RES: " + currRes);
-					System.out.println("Peso resultante: " + pesoDaRota(tempRes.getList()));
-					System.out.println("FIM: " + fim + "TESTFIM Rota: " + tempRes.getLast());
-					if(pesoDaRota(tempRes.getList()) < pesoDaRota(currRes) || currRes.isEmpty() ) {
+				// System.out.println(String.format("Estou em linha %d coluna %d com peso %d\n",
+				// inicio, i, lin[i].getPeso()));
+				// System.out.println("Pushed! TEMPRES: " + tempRes);
+				if (tempRes.getLast().getChegada() - 1 == fim) {
+					// System.out.println("Achei rota: " + tempRes + "RES: " + currRes);
+					// System.out.println("Peso resultante: " + pesoDaRota(tempRes.getList()));
+				//	System.out.println("FIM: " + fim + " FIM Rota: " + tempRes.getLast().getSaida());
+					if (pesoDaRota(tempRes.getList()) < pesoDaRota(currRes) || currRes.isEmpty()) {
 						currRes.clear();
 						System.out.println("Achei rota: " + tempRes);
 						for (Rota rota : tempRes.getList()) {
@@ -74,7 +79,7 @@ public class Dijkstra {
 					tempRes.pop();
 					lin[i].setFlag(false);
 					continue;
-				}else {
+				} else {
 					resolve(i, tempRes, currRes);
 					tempRes.pop();
 					lin[i].setFlag(false);
@@ -85,59 +90,58 @@ public class Dijkstra {
 
 	}
 
-	public Rota[][] initMatriz(Rota[] rotas){
+	public Rota[][] initMatriz(Rota[] rotas) {
 		Rota res[][];
 		int size = 0;
 
 		for (Rota r : rotas)
-			if(r.getChegada() > size)
+			if (r.getChegada() > size)
 				size = r.getChegada();
 
-		res = new Rota[size][size ];
+		res = new Rota[size][size];
 
 		for (Rota r : rotas) {
 			res[r.getSaida() - 1][r.getChegada() - 1] = r;
-			res[ r.getChegada() - 1][r.getSaida() - 1] = r;	///TODO: Perguntar na aula se precisa da matriz espelhada
-			//System.out.println(r);
+			res[r.getChegada() - 1][r.getSaida() - 1] = r;
+			// System.out.println(r);
 		}
 
-
-		///TODO: Checar se a formatacao fica boa
-		/**for(int i = 0; i < 21; i++)
+		/// TODO: Checar se a formatacao fica boa
+		for (int i = 0; i < 21; i++)
 			System.out.print(i + "\t");
 		System.out.println();
 		for (int i = 0; i < res.length; i++) {
 			System.out.print(i + ": ");
 			for (int j = 0; j < res[i].length; j++) {
-				if(res[i][j] == null)
+				if (res[i][j] == null)
 					System.out.print("\t0\t|");
 				else
 					System.out.print("\t" + res[i][j].getPeso() + "\t|");
 			}
-			System.out.println("\n\t----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-		}*/
+			System.out.println(
+					"\n\t----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		}
 		return res;
 	}
 
 	public void mostraResposta(ArrayList<Rota> rotas, StringBuilder res, int i) {
 
 		Rota tmp1 = null, tmp2 = null;
-		if(i + 1 < rotas.size())
-			if(rotas.get(i).getCor().equals(rotas.get(i + 1).getCor()) ) {
+		if (i + 1 < rotas.size())
+			if (rotas.get(i).getCor().equals(rotas.get(i + 1).getCor())) {
 				tmp1 = rotas.get(i);
 				tmp2 = rotas.remove(i + 1);
 				tmp1.setChegada(tmp2.getChegada());
 				tmp1.setPeso(tmp1.getPeso() + tmp2.getPeso());
 				mostraResposta(rotas, res, i++);
 			}
-			if(!rotas.isEmpty()) {
-				tmp1 = rotas.remove(i);
-				res.append(String.format("Pegar linha %s de %d a %d (%d)\n", tmp1.getCor(), tmp1.getSaida(), tmp1.getChegada(), tmp1.getPeso()));
-				mostraResposta(rotas, res, i++);
-				}
+		if (!rotas.isEmpty()) {
+			tmp1 = rotas.remove(i);
+			res.append(String.format("Pegar linha %s de %d a %d (%d)\n", tmp1.getCor(), tmp1.getSaida(),
+					tmp1.getChegada(), tmp1.getPeso()));
+			mostraResposta(rotas, res, i++);
+		}
 
 	}
-
-
 
 }
